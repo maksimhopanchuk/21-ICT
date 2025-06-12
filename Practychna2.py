b020981dca1a4e81e01bcf33d709451e08cfbdcc
maksimhopanchuk <maksimgopanchuk74@gmail.com>
2025-06-12 23:19:54 +0300

    Add files via upload

---
+++ Practychna2.py
@@ -0,0 +1,22 @@
+class StudentInfo(Name):n
+    def __init__(self, name=None, surname=None, birth_year=None, city=None, university=None, email=Noe):
+        super().__init__(name, surname, birth_year)
+        self.city = city
+        self.university = university
+        self.email = email
+
+    def _is_adult(self):
+        if self.birth_year is None:
+            return False
+        return (2025 - self.birth_year) >= 18
+
+    def get_full_info(self):
+        return {
+            "name": self.name,
+            "surname": self.surname,
+            "birth_year": self.birth_year,
+            "city": self.city,
+            "university": self.university,
+            "email": self.email,
+            "adult": self._is_adult()
+        }
